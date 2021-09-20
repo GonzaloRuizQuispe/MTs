fc8c262ef384cbe00f30d6cae9a4ed544b9be526
ChachaJr <62969610+GonzaloRuizQuispe@users.noreply.github.com>
2021-09-20 00:34:48 -0300

    Actualizacion

--- Conexion.py
+++ Conexion.py
@@ -1,15 +1,2 @@
 import mysql.connector
 from mysql.connector import Error
-
-try:
-    conexion = mysql.connector.connect(
-        host='localhost',
-        port=3306,
-        user='root',
-        password='123456',
-        db='condominio'
-    )
-    if conexion.is_connected():
-        print("Conectado exitosamente")
-except Error as ex:
-    print("Error durante la conexion", ex)
