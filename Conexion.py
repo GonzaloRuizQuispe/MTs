5c2d5db6cb653ac95e9ae8737a02937c94e79ac8
ChachaJr <62969610+GonzaloRuizQuispe@users.noreply.github.com>
2021-09-22 20:08:37 -0300

    remote control

--- Conexion.py
+++ Conexion.py
@@ -1,2 +1,15 @@
+from _typeshed import Self
 import mysql.connector
 from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(
+        host = 'localhost',
+        port = 3306,
+        user = 'root',
+        password = 'Pool!Live45427752',
+        db = 'mts'
+    )
+    
+except Exception as ex:
+    print(ex)
