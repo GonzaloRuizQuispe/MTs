ab751717a396c7afd05d22d363e3c4506f380130
ChachaJr <62969610+GonzaloRuizQuispe@users.noreply.github.com>
2021-09-23 22:38:04 -0300

    Data Base --Upgrade

--- DB_Diseases.sql
+++ DB_Diseases.sql
@@ -1,10 +1,22 @@
-CREATE DATABASE db_diseases
+CREATE DATABASE db_diseases;
 
-CREATE TABLE diseases (
-	id_diseases INT AUTO_INCREMENT NOT NULL,
-	name VARCHAR (250) NOT NULL,
-    trataments VARCHAR (250) NOT NULL,
-    causes VARCHAR (250) NOT NULL,
-    symptom VARCHAR (250) NOT NULL,
-    PRIMARY KEY (id_diseases)
-)
+CREATE TABLE enfermedad (
+	id INT NOT NULL AUTO_INCREMENT,
+    Nombre VARCHAR (50) NOT NULL,
+	PRIMARY KEY (id,Nombre)
+);
+
+CREATE TABLE tratamientos (
+	Tratamiento_id INT NOT NULL AUTO_INCREMENT,
+    Tratamiento VARCHAR (275) NOT NULL,
+    Nombre_E VARCHAR (50) NOT NULL,
+    PRIMARY KEY (Tratamiento_id),
+    CONSTRAINT fk_Tratamiento_Enfermedad FOREIGN KEY (Nombre_E) REFERENCES enfermedad (Nombre)
+);
+
+INSERT INTO enfermedad (Nombre,Sintomas,Tratamientos,Causas) VALUES
+('Ulcera Corneal','Lagrimeo-Escozor-Irritacion-Enrojecimiento-Dolor_De_Cabeza-Vision_Borrosa',
+'Proteger Los Ojos De Forma Adecuada Y Unos Primeros Auxilios A Tiempo Pueden Salvar La Vista',
+'Traumatismo_directos-Cuerpos_extraños');
+
+SELECT * FROM enfermedad;
