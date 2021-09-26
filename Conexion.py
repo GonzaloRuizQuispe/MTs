262162b41a9268a2f911e57e5e50dfa7138824c1
ChachaJr <62969610+GonzaloRuizQuispe@users.noreply.github.com>
2021-09-25 21:15:23 -0300

    Finish connections to database

--- Conexion.py
+++ Conexion.py
@@ -9,6 +9,15 @@ try:
         password = 'Pool!Live45427752',
         db = 'db_diseases'
     )
+    if connection.is_connected():
+        print("Conexion Exitosa")
+        infoServer = connection.get_server_info()
+        print("Info del servidor:",infoServer)
     
-except Exception as ex:
-    print(ex)
+except Error as ex:
+    print("Error durante la conexion: ",ex)
+
+finally:
+    if connection.is_connected():
+        connection.close()
+        print("La conexion ha finalizado.")
