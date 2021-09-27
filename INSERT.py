ef19c746d78ab114365913a31885b336bf27afeb
ChachaJr <gonzaloruizquispe@gmail.com>
2021-09-27 16:32:18 -0300

    Websites --upgrade MTs, My Account, About

--- INSERT.py
+++ INSERT.py
@@ -1,4 +1,15 @@
 #Nombre de variblaes a cambiar para la web
 
 def INSERT (vector,cursor):
-    return cursor.execute("INSERT INTO enfermedad (nombre) VALUES ('vector')")
+     cursor.execute("INSERT INTO enfermedad (nombre) VALUES ("+vector+")")
+
+def Insertar(palabra):
+    print(palabra)
+
+def main():
+    EPEP=input(str("Ingrese la palabra:  "))
+    Insertar(EPEP)
+
+
+#Para guardar un valor con la sentencia mysql se necesita que si o si sea "str" y para comparar se debe mostrar los valores
+#para saber como se guardan y entonces empezar a comparar
