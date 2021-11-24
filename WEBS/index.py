9e25f0ca82b246e5cb3a814fbfd863842664974c
ChachaJr <62969610+GonzaloRuizQuispe@users.noreply.github.com>
2021-11-24 00:26:47 -0300

    login incomplete

--- WEBS/index.py
+++ WEBS/index.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from re import S, X
+from flask import Flask, render_template, request, redirect, url_for, session
+import pyotp
+from pyotp import totp
+
+#Librerias Propias
+secret = pyotp.random_base32()
+print("Mi Secreto:", secret)
+totp_object = pyotp.TOTP(secret)
+qr_text = totp_object.provisioning_uri(name="Administrador", issuer_name="MTs")
+print(qr_text)
+#----------------
 
 app = Flask(__name__)
 
-
 @app.route('/Home')
 def Home():
+
     return render_template('Hofice.html')
 
 
@@ -18,9 +29,20 @@ def info():
     return render_template('Info.html')
 
 
-@app.route('/Login')
+@app.route('/Login', methods = ["GET","POST"])
 def Login():
-    return render_template('Login.html')
+    if request.method == 'POST':
+
+        code = request.form['password']
+        totp_object = pyotp.TOTP(secret)
+        valid = totp_object.verify(code)
+        print(code)
+        if valid == 'true':
+            return render_template('Hofice.html')
+        else:
+            return render_template('Login.html')
+    else:
+        return render_template('Login.html')
 
 
 if __name__ == '__main__':
